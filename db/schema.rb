3525080eb5dd599edc603976f0fefbda7894d8e9
CollegeCity <kroolikk@gmail.com>
2013-02-19 00:30:12 +0400

    added social media to articles

--- db/schema.rb
+++ db/schema.rb
@@ -89,8 +89,8 @@ ActiveRecord::Schema.define(:version => 20130213191418) do
   add_index "posts", ["slug"], :name => "index_posts_on_slug", :unique => true
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",     :null => false
+    t.string   "encrypted_password",     :default => "",     :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -99,12 +99,12 @@ ActiveRecord::Schema.define(:version => 20130213191418) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "role"
+    t.string   "role",                   :default => "user"
     t.string   "slug"
     t.string   "name"
     t.string   "avatar"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
     t.string   "image"
   end
 
