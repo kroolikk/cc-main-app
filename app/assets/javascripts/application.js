6e040359abe7ce19ac8875814fee6b4c0c737ea6
CollegeCity <kroolikk@gmail.com>
2013-03-17 13:37:15 +0400

    changed order of includes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,8 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require blur
-//= require init
 //= require masonry
 //= require slides.jquery
 //= require_tree .
 //= require ckeditor/init
+//= require init
