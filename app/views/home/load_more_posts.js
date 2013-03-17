f49c88897c629d10e3b9dc48d2c3871411f84676
Mateusz <kroolikk@gmail.com>
2013-03-17 11:30:18 +0100

    improved endless loading

--- app/views/home/load_more_posts.js
+++ app/views/home/load_more_posts.js
@@ -1,3 +1,6 @@
 var $content = "<%= escape_javascript( render( :partial => 'shared/post_box') ) %>";
 $('.post_box').append($content);
 $('.mason-holder').masonry( 'reload' );
+
+enable_loading = true;
+currentPage += 1;
